0c147679a4bb48e285c24c86a34f10e4bc61277a
牛玉龙 <niuyulong@jd.com>
2018-09-29 18:00:50 +0800

    initial commit

---
+++ TestVisibleCells/TestVisibleCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestVisibleCells
+//
+//  Created by niuyulong on 2018/9/29.
+//  Copyright © 2018年 nyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestVisibleCells/TestVisibleCells/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestVisibleCells
+//
+//  Created by niuyulong on 2018/9/29.
+//  Copyright © 2018年 nyl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
